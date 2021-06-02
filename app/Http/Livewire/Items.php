496191027df38154af0ad6f987b20ff12002b652
cristian16b <cristian.budzicz@gmail.com>
2021-06-02 19:16:39 -0300

    Agrego metodos para el ordenamiento, crud de los items

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -54,5 +54,67 @@ class Items extends Component
         ));
     }
 
+    public function updatingActive() 
+    {
+        $this->resetPage();
+    }
+ 
+    public function updatingQ() 
+    {
+        $this->resetPage();
+    }
+
+    public function sortBy( $field) 
+    {
+        if( $field == $this->sortBy) {
+            $this->sortAsc = !$this->sortAsc;
+        }
+        $this->sortBy = $field;
+    }
+ 
+    public function confirmItemDeletion( $id) 
+    {
+        $this->confirmingItemDeletion = $id;
+    }
+ 
+    public function deleteItem( Item $item) 
+    {
+        $item->delete();
+        $this->confirmingItemDeletion = false;
+        session()->flash('message', 'Item Deleted Successfully');
+    }
+ 
+    public function confirmItemAdd() 
+    {
+        $this->reset(['item']);
+        $this->confirmingItemAdd = true;
+    }
+ 
+    public function confirmItemEdit(Item $item) 
+    {
+        $this->resetErrorBag();
+        $this->item = $item;
+        $this->confirmingItemAdd = true;
+    }
+ 
+    public function saveItem() 
+    {
+        $this->validate();
+ 
+        if( isset( $this->item->id)) {
+            $this->item->save();
+            session()->flash('message', 'Item Saved Successfully');
+        } else {
+            auth()->user()->items()->create([
+                'name' => $this->item['name'],
+                'price' => $this->item['price'],
+                'status' => $this->item['status'] ?? 0
+            ]);
+            session()->flash('message', 'Item Added Successfully');
+        }
+ 
+        $this->confirmingItemAdd = false;
+ 
+    }
 
 }
